b97c0b8e54ff6f9cf87b53f2204a732e0ee96dec
Miguel Pereira <141651248+Miguel1DM@users.noreply.github.com>
2024-04-25 11:32:12 -0300

    Delete Js directory

--- Js/index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('btnLogin').addEventListener('click', function (){
-    let email = document.getElementById('email').value;
-    let senha = document.getElementById('senha').value;
-    
-})
