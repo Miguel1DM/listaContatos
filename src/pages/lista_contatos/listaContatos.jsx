e9ffc3028db6521e02fbdff0c394e59ed5e57232
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2024-04-30 11:39:28 -0300

    fix and feat
    add feature on login page, to login user, and fix contact list css

--- src/pages/lista_contatos/listaContatos.jsx
+++ src/pages/lista_contatos/listaContatos.jsx
@@ -1,98 +1,71 @@
-import { React, useState,} from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPhoneVolume, faTrashAlt, faUser, faCirclePlus} from "@fortawesome/free-solid-svg-icons"
-import './listacontatos.css'
+import { React, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhoneVolume,
+  faTrashAlt,
+  faUser,
+  faCirclePlus,
+  faList,
+} from "@fortawesome/free-solid-svg-icons";
+import "./listacontatos.css";
+import jwt from 'json'
+
+// Biblioteca para fazer requisições
+import axios from 'axios'
 
 // Importando Componente de Contato
-import Contato from '../../Componentes/contato/contato'
+import Contato from "../../Componentes/contato/contato";
 
 function ListaContatos(props) {
-    // Estados 
-    const [setListaContatos, listaContatos] = useState([]);
-    const [contato, setContato] = useState({ id: '', nome: '', telefone: '' });
+  // Busancdo lista de Usuários
+  const idUsuario;
+  const contacts = axios.get(`https://apicontato.onrender.com/contatos/${idUsuario}/`)
+  const [setListaContatos, listaContatos] = useState([]);
 
+  // Contato a ser adicionado
+  const [contato, setContato] = useState({ id: "", nome: "", telefone: "" });
 
-    // Funções de estado do componente a ser inserido
-    function changeInputNome(event) {
-        setContato({ ...contato, nome: event.target.value })
-      }
-    function changeInputTelefone(event) {
-        setContato({ ...contato, telefone: event.target.value })
-      }
-    
-return (
-    <div className="mx-2" id="Salada">
+  // Funções de estado do componente a ser inserido
+  function changeInputNome(event) {
+    setContato({ ...contato, nome: event.target.value });
+  }
+  function changeInputTelefone(event) {
+    setContato({ ...contato, telefone: event.target.value });
+  }
+
+  return (
+    <div className="mx-2">
+      <h2 className="text-center mt-3">
+        <FontAwesomeIcon icon={faList} className="me-3" />
+        Contact Post
+      </h2>
+      <form action="" className="formContact">
+        <div className="mb-3">
+          <label className="form-label">Nome</label>
+          <input
+            type="text"
+            onChange={changeInputNome}
+            value={contato.nome}
+            className="form-control"
+          />
+        </div>
         <div>
-            <div className="listadecontatos">
-              <div className="container-fluid titulo">
-                <div className="row">
-                  <div className="col text-center">
-                    <h2 className="text-center"><FontAwesomeIcon className="me-3" />Contact Post</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="container-fluid formulario">
-                <div className="row">
-                  <div className="col p-3">
-                    <div className="row justify-content-center">
-                      <div className="col-10 col-sm-8 col-md-6 col-lg-4 ">
-                        <div className="mb-3">
-                          <label className="form-label">Nome</label><br />
-                          <input type="text" onChange={changeInputNome} value={contato.nome} className="form-control" />
-                        </div>
-                        <div>
-                          <label className="form-label">Telefone</label><br />
-                          {/* Adicionar estado de clicar enter "onKeyUp={enterAdicionarContato}" */}
-                          <input type="text" onChange={changeInputTelefone} value={contato.telefone} className="form-control" />
-                        </div>
-                        <div className="row mt-3">
-                          <div className="col text-end">
-                            {/* Adicionar serviço para adicionar Contato  */}
-                            <button className="btn btn-outline-primary">
-                              <FontAwesomeIcon icon={faCirclePlus} className="me-2" />
-                              Adicionar
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <ul>
-                      {/* {listaContatos.map(ct => {
-                        // Adicionar serviço para remover contato pelo ID "remover={removerContato}"
-                        return <Contato key={ct.id} id={ct.id} nome={ct.nome} telefone={ct.telefone} />
-                      })} */}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <label className="form-label">Telefone</label>
+          {/* Adicionar estado de clicar enter "onKeyUp={enterAdicionarContato}" */}
+          <input
+            type="text"
+            onChange={changeInputTelefone}
+            value={contato.telefone}
+            className="form-control"
+          />
         </div>
-        <div className="container componente-contato my-4">
-            <div className="row">
-                <div className="col p-2">
-                    <h4>
-                        <FontAwesomeIcon icon={faUser} className="me-3"></FontAwesomeIcon>
-                        {props.nome}
-                    </h4>
-                </div>
-                <div className="col p-2">
-                    <h4>
-                        <FontAwesomeIcon icon={faPhoneVolume} className="me-3"></FontAwesomeIcon>
-                        {props.telefone}
-                    </h4>
-                </div>
-                <div className="col p-2 text-end">
-                    <h4>
-                        <FontAwesomeIcon
-                            icon={faTrashAlt}
-                            className="me-3"
-                            onClick={() => { props.remover(props.id) }}></FontAwesomeIcon>
-                    </h4>
-                </div>
-            </div>
-        </div>
+        <button className="btn btn-primary mt-4" >
+          <FontAwesomeIcon icon={faCirclePlus} className="me-2" />
+          Adicionar
+        </button>
+      </form>
     </div>
-    )
+  );
 }
 
 export default ListaContatos;
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,14 +1,53 @@
 import './login.css';
 
-// Componentes do React
-import { React, Link } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 // Importando Assets
 import imgLogin from '../../img/imgLogin.png';
 
-function Login() {
-  function authenticarUsuario(){
-    console.log("Fazendo Login!!")
+// Biblioteca para fazer requisições
+import axios from 'axios'
+
+export default function Login() {
+
+  // Função de Autenticação de Login
+  async function AuthLogin(){
+    const navigate = useNavigate();
+
+    const email = document.getElementById("inputEmail").value
+    const senha = document.getElementById("inputSenha").value
+    let res = document.getElementById("res")
+    res.innerHTML = "";
+
+    // Validando campos a ser inseridos 
+    if(!email || !senha){
+      res.innerHTML = "Insira um Email e senha.";
+    }else{
+      const data = {
+        "email": `${email}`,
+        "senha": `${senha}`
+      }
+  
+      // Busca o email e senha do usuário na API
+      const response = await axios.post("https://apicontato.onrender.com/login",data)
+        if(response.status === 200){
+          const statusLogin = response.data.result[0].statusLogin
+          if(statusLogin){
+            // Salvando Token no localStorage
+            const token = response.data.result[0].token;
+            localStorage.setItem("token",`${token}`)
+
+            res.innerHTML = "Realizando Login..."
+            navigate('/listacontatos');
+          }else{
+            res.innerHTML = "Email ou Senha incorretos..."
+          }
+        }else{
+          res.innerHTML = "Erro ao buscar Usuário."
+        }
+    }
   }
 
   return (
@@ -22,18 +61,17 @@ function Login() {
         <p className='subtitulo'>Faça login para acessar sua lista de contatos</p>
         <div className='email'>
           <p>E-mail</p>
-          <input className='inputEmail' type='text'></input>
+          <input id="inputEmail" className='inputEmail' type='text'></input>
         </div>
         <div className='senha'>
           <p>Senha</p>
-          <input className='inputSenha' type='password'></input>
+          <input id="inputSenha" className='inputSenha' type='password'></input>
         </div>
-        <p className='esquecerSenha'>Esqueceu a sua senha? <Link to="/nova_senha" className='esquecerSenha_link'>Clique aqui</Link></p>
-        <Link to="/listacontatos" className='btnLogin' onClick={authenticarUsuario}>Login</Link>
-        <p className='cliqueCadastrar'>Não tem uma conta? <Link to="/cadstro" className='cliqueCadastrar_link'>Cadastre-se agora</Link></p>
+        <p className='esquecerSenha'>Esqueceu a sua senha? <Link to="/novasenha" className='esquecerSenha_link'>Clique aqui</Link></p>
+        <button onClick={AuthLogin} className='btnLogin'>Login</button>
+        <p id="res"></p>
+        <p className='cliqueCadastrar'>Não tem uma conta? <Link to="/cadastro" className='cliqueCadastrar_link'>Cadastre-se agora</Link></p>
       </div>
     </div>
   );
 }
-
-export default Login;
