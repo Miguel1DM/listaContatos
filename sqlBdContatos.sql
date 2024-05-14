6a7cd9fdba2447d143ba4c5498a692fd107b87b6
Miguel Pereira <141651248+Miguel1DM@users.noreply.github.com>
2024-05-14 08:18:35 -0300

    Update sqlBdContatos.sql

--- sqlBdContatos.sql
+++ sqlBdContatos.sql
@@ -1,21 +1,23 @@
-CREATE DATABASE listaContatos;
+CREATE DATABASE listacontatos;
 
-USE listaContatos;
+USE listacontatos;
 
-CREATE TABLE usuario(
+CREATE TABLE usuario (
 	id SMALLINT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(20) NOT NULL,
-    email VARCHAR(80) NOT NULL UNIQUE,
-    senha VARCHAR(30) NOT NULL
+    	nome VARCHAR(50) NOT NULL,
+    	email VARCHAR(80) NOT NULL,
+    	senha VARCHAR(15) NOT NULL,
+    	verificacao BOOLEAN,
+    	codigoVerificacao SMALLINT(5)
 );
 
 CREATE TABLE contato(
 	id SMALLINT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(20) NOT NULL,
-    endereco VARCHAR(100) NOT NULL,
-    telefone VARCHAR(20) NOT NULL,
-    email VARCHAR(80) NOT NULL,
-    idUsuario SMALLINT NOT NULL,
-    FOREIGN KEY (idUsuario) REFERENCES usuario(id) 
+    	nome VARCHAR(20) NOT NULL,
+    	endereco VARCHAR(100) NOT NULL,
+    	telefone VARCHAR(20) NOT NULL,
+    	email VARCHAR(80) NOT NULL,
+    	idUsuario SMALLINT NOT NULL,
+    	FOREIGN KEY (idUsuario) REFERENCES usuario(id) 
 );
 
